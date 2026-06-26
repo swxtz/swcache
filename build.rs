c14f8cb81188f78dd4e463eba20864c5a5ee4b15
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2026-06-25 23:48:42 -0300

    feat: create grpc server

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::compile_protos("src/protobuf/proto/ping.proto")?;
+    tonic_prost_build::configure()
+        .build_client(true)
+        .compile_protos(&["src/protobuf/proto/ping.proto"], &["proto"])?;
     Ok(())
 }
